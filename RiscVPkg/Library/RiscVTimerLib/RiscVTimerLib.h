1dacd8e5343cc4ab47517beec496e8edbf2b2941
Abner Chang <abner.chang@hpe.com>
2019-09-23 08:31:43 +0800

    RiscVPkg/Library: Add RISC-V timer library
    Timer library for RISC-V
    
    Signed-off-by: Abner Chang <abner.chang@hpe.com>

---
+++ RiscVPkg/Library/RiscVTimerLib/RiscVTimerLib.h
@@ -0,0 +1,21 @@
+/** @file
+  RISC-V timer library definitions.
+
+  Copyright (c) 2016 - 2019, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+
+#ifndef _RISCV_TIMER_LIB_INTERNAL_H_
+#define _RISCV_TIMER_LIB_INTERNAL_H_
+
+#include <Uefi.h>
+#include <Library/PcdLib.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseLib.h>
+
+#include <Library/RiscVCpuLib.h>
+
+#endif // _RISCV_TIMER_LIB_INTERNAL_H_
