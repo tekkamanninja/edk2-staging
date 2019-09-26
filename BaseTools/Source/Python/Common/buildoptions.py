fe635029c0349011417d4a66f010d5273bd2b47c
Leif Lindholm <leif.lindholm@linaro.org>
2019-09-26 09:09:13 -0700

    BaseTools: add RISCV64 architecture to build command(s)
    Signed-off-by: Leif Lindholm <leif.lindholm@linaro.org>

--- BaseTools/Source/Python/Common/buildoptions.py
+++ BaseTools/Source/Python/Common/buildoptions.py
@@ -25,7 +25,7 @@ def SingleCheckCallback(option, opt_str, value, parser):
 
 def MyOptionParser():
     Parser = OptionParser(description=__copyright__, version=__version__, prog="build.exe", usage="%prog [options] [all|fds|genc|genmake|clean|cleanall|cleanlib|modules|libraries|run]")
-    Parser.add_option("-a", "--arch", action="append", type="choice", choices=['IA32', 'X64', 'EBC', 'ARM', 'AARCH64'], dest="TargetArch",
+    Parser.add_option("-a", "--arch", action="append", type="choice", choices=['IA32', 'X64', 'EBC', 'ARM', 'AARCH64', 'RISCV64'], dest="TargetArch",
         help="ARCHS is one of list: IA32, X64, ARM, AARCH64 or EBC, which overrides target.txt's TARGET_ARCH definition. To specify more archs, please repeat this option.")
     Parser.add_option("-p", "--platform", action="callback", type="string", dest="PlatformFile", callback=SingleCheckCallback,
         help="Build the platform specified by the DSC file name argument, overriding target.txt's ACTIVE_PLATFORM definition.")
